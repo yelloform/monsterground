5220df827dad369d2f50585bcb376aeb1bb605fd
ovo1381 <hijbulbd@gmail.com>
2017-12-27 15:27:17 +0900

    애니메이션 추가

--- js/index.js
+++ js/index.js
@@ -1,12 +1,39 @@
-/*$(document).ready(function(){
-    $('.home-arrow-center').mouseenter(function(){
-        $('.home-arrow-center').hide();
-        $('.home-arrow-center-hover').fadeIn(100);
-    });
-    $('.home-arrow-center-hover').mouseleave(function(){
-        $('.home-arrow-center').fadeIn(100);
-        $('.home-arrow-center-hover').hide();
-    });
-    
-    
-});*///end
+$(document).ready(function() {
+            $('#fullpage').fullpage({
+                anchors: ['section0', 'section1', 'section2', 'section3', 'section4', 'section5'],
+                menu: '.main_menu',
+                scrollingSpeed: 300,
+                slidesNavigation: true,
+                paddingTop: 0,
+                paddingBottom: 0,
+                scrollOverflow:true,
+                afterLoad: function(anchorLink, index){
+                    var loadedSection = $(this);
+
+                    if(index == 1){
+                      $('.title-wrap h2').hide();
+
+                    }
+                    if(index == 2){
+                        $('.title-wrap h2').show();
+                      TweenLite.from('.title-wrap h2',1,{opacity:1, top:"-20px", delay:0.5 })
+
+                    }
+
+
+                },
+                onLeave: function(index, nextIndex, direction) {
+                    var loadedSection = $(this);
+                }
+            });
+            $('#pofol01').appendTo("body");
+            $('#pofol02').appendTo("body");
+            $('#pofol03').appendTo("body");
+            $('#pofol04').appendTo("body");
+            $('#pofol05').appendTo("body");
+            $('#pofol06').appendTo("body");
+            $('#pofol07').appendTo("body");
+            $('#pofol08').appendTo("body");
+            $('#pofol09').appendTo("body");
+            $('#pofol10').appendTo("body");
+        });
